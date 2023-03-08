e5b5344e9facbaf9cf1cf344efbc9fad5254f272
deVerasRodrigo <rodrigo.veras37@gmail.com>
2023-03-08 18:55:55 -0300

    Initial commit

---
+++ src/main/java/br/com/ada/LocateCar_sb/LocateCarSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.LocateCar_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocateCarSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocateCarSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/LocateCar_sb/LocateCarSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.LocateCar_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocateCarSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
