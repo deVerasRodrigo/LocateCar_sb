c1f324e72745e51eaad9b7683be5c52f51c25e4b
deVerasRodrigo <rodrigo.veras37@gmail.com>
2023-03-08 18:59:55 -0300

    Criado pacote controller

---
+++ src/main/java/br/com/ada/LocateCar_sb/controller/VeiculoController.java
@@ -0,0 +1,39 @@
+package br.com.ada.LocateCar_sb.controller;
+
+import br.com.ada.programacaowebIsb.model.Veiculo;
+import br.com.ada.programacaowebIsb.service.VeiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class VeiculoController {
+
+    @Autowired
+    private VeiculoService veiculoService;
+
+    @GetMapping("/veiculos")
+    public String veiculos(Model model) {
+        List<Veiculo> veiculos = this.veiculoService.listarTodos();
+        model.addAttribute("veiculos", veiculos);
+        return "veiculos";
+    }
+
+    @GetMapping("/veiculo/add")
+    public String addVeiculo(Model model) {
+        model.addAttribute("veiculo", new Veiculo());
+        return "veiculo-add";
+    }
+
+    @PostMapping("/veiculo/add")
+    public String criarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo) {
+        this.veiculoService.createVeiculo(veiculo);
+        return "redirect:/veiculos";
+    }
+
+}
